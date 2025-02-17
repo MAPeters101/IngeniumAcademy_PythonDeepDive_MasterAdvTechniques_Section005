bcf78f91e61cf05db89a14fcf00e813fbf6a3f0c
Mark Peters <mapeters101@earthlink.net>
2025-02-17 18:32:50 -0500

    Complete work on lesson020 from 3:30. __main__ to test count_up_to()

--- lesson020.py
+++ lesson020.py
@@ -6,15 +6,21 @@ def count_up_to(n):
 
 
 if __name__ == '__main__':
-    counter = count_up_to(5)
-    # print(counter[3])
-    print(next(counter)) # 1
-    print(next(counter)) # 2
+    # counter = count_up_to(5)
+    # # print(counter[3])
+    # print(next(counter)) # 1
+    # print(next(counter)) # 2
+    #
+    # for num in counter: # 3, 4, 5
+    #     print(num)
+    #
+    # for num in counter: # Nothing generator
+    #     print(num)
 
-    for num in counter: # 3, 4, 5
+    # # Generator Expressions
+    squared_numbers = (x * x for x in range(5))
+    print(next(squared_numbers)) # 0
+    print(next(squared_numbers)) # 1
+
+    for num in squared_numbers:
         print(num)
-
-    for num in counter: # Nothing generator
-        print(num)
-
-
