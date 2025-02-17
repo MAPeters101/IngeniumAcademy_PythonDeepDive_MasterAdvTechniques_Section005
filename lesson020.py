e6509155c28454b154e33974256e385f29d9bda0
Mark Peters <mapeters101@earthlink.net>
2025-02-17 18:29:24 -0500

    Work on lesson020 to 1:40 3:30. __main__ to test count_up_to()

--- lesson020.py
+++ lesson020.py
@@ -1,7 +1,20 @@
-def count_uo_to(n):
+def count_up_to(n):
     count = 1
     while count <= n:
         yield count # [1, 2, 3, 4, 5, 6]
         count += 1
+
+
+if __name__ == '__main__':
+    counter = count_up_to(5)
+    # print(counter[3])
+    print(next(counter)) # 1
+    print(next(counter)) # 2
+
+    for num in counter: # 3, 4, 5
+        print(num)
+
+    for num in counter: # Nothing generator
+        print(num)
 
 
