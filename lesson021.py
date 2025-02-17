88480c2e788a68863bb890294e4c75806a7b0305
Mark Peters <mapeters101@earthlink.net>
2025-02-17 18:39:05 -0500

    Work on lesson021 from 0:30 to 50. __main__ for infinite_sequence()

--- lesson021.py
+++ lesson021.py
@@ -1,7 +1,12 @@
-def infinite_sequence()
+def infinite_sequence():
     num = 0
     while True:
         yield num
         num += 1
 
+
+if __name__ == '__main__':
+    infinite = infinite_sequence()
+    print(next(infinite)) # 0
+    print(next(infinite)) # 1
 
